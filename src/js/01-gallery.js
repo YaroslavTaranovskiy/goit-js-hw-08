1d22603040b7eef2e17fe93ca423cedb334f511e
YaroslavTaranovskiy <103592817+YaroslavTaranovskiy@users.noreply.github.com>
2022-09-20 21:16:32 +0300

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,37 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
+
+const imgContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+imgContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+
+function createGalleryCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+
+        return `
+        <a class="gallery__item" 
+        href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" 
+  />
+</a>
+        `;
+    })
+        .join('');
+}
+
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: 'bottom',
+        
+});
+
+
 console.log(galleryItems);
