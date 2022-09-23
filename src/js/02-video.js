56034cf55c8f842bba89956e75cff6db8db59412
YaroslavTaranovskiy <103592817+YaroslavTaranovskiy@users.noreply.github.com>
2022-09-23 15:09:16 +0300

    task -2 done( after update)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -12,30 +12,14 @@ player.on('timeupdate', throttle(checkTime, 1000));
 
 
 function checkTime(e) {
-    const message = JSON.stringify(e);
-    localStorage.setItem(LOCLSTORAGE_KEY, message);
+    // const message = JSON.stringify(e);
+    localStorage.setItem(LOCLSTORAGE_KEY, e.seconds);
 }
 
 const time = localStorage.getItem(LOCLSTORAGE_KEY);
 
 if (time !== null) {
-    const currentTime = JSON.parse(time);
-    const seconds = currentTime.seconds;
-
-player
-    .setCurrentTime(seconds)
-    .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-    })
-    .catch(function (error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
+    player.setCurrentTime(time);
 }
+
+
